8bf3ed47d3d852bf46f044fcfbf2692c17eff3ed
mouadd55 <mouad_oudrib@outlook.com>
2023-08-15 23:56:09 +0100

    Data Structures: Lists, Tuples: Add task 5 & task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for column in range(len(row)):
             print(
-                "{:d}".format(column),
+                "{:d}".format(row[column]),
                 end="" if column == len(row) - 1 else " ")
         print()
