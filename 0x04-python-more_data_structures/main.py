8c3a76eddd637c1f3db1a67acb79e32fd1375ec8
mouadd55 <mouad_oudrib@outlook.com>
2023-08-17 00:43:08 +0100

    Data Structures: Set, Dictionary : Add task 7 & 8

--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
-search_replace = __import__('1-search_replace').search_replace
+simple_delete = __import__('8-simple_delete').simple_delete
+print_sorted_dictionary = \
+    __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3] }
+new_dict = simple_delete(a_dictionary, 'track')
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
 
-print(new_list)
-print(my_list)
+print("--")
+print("--")
+new_dict = simple_delete(a_dictionary, 'c_is_fun')
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
