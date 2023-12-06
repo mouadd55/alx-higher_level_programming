7a79f31e99544cd44b3049bfc23831a99a2a212a
mouadd55 <mouad_oudrib@outlook.com>
2023-12-06 03:15:08 +0100

    Add 12-switch.py

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,34 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - check if a linked list is a palindrome
+ * @head: first node
+ * Return: 1 if success, 0 in case it failed
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *tmp = *head;
+	int content[2048];
+    int i = 0;
+    int idx, max;
+
+	if (!head || !(*head))
+		return (1);
+	while (tmp)
+	{
+		content[i++] = tmp->n;
+		tmp = tmp->next;
+	}
+    if (i % 2 == 0)
+	    max = i / 2;
+    else
+        (i + 1) / 2;
+	for (idx = 0; idx < max; idx++)
+    {
+		if (content[idx] != content[i - 1 - idx])
+			return (0);
+    }
+	return (1);
+}
